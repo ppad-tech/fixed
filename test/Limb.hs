d53a9cdc30e537b10308913256af15052394f588
Jared Tobin <jared@jtobin.io>
2025-11-30 11:05:46 +0400

    test: re-add skeleton

---
+++ test/Limb.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+module Limb where
+
+import Test.Tasty
+import qualified Test.Tasty.HUnit as H
+import qualified Test.Tasty.QuickCheck as Q
+import qualified Data.Word.Limb as L
+
